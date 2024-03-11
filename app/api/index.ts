d226b6bb555b41de634dca02585d07cc59fe69c2
keno <keno@thinkbitsolutions.com>
2024-03-11 12:44:26 +0800

    fix: env values

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,6 +1,6 @@
 const data = {
 	// Domain 
-	domain: process.env.NEXT_PUBLIC_BASE_URL ?? "https://beta.sandsofarrakis.me/",
+	domain: process.env.NEXT_PUBLIC_BASE_URL ?? "https://vana-dune-app.vercel.vana.com/",
 
 	// API Url
 	api_url: process.env.NEXT_PUBLIC_API_URL ?? "https://beta.sandsofarrakis.me/api",
--- app/utils/common.ts
+++ app/utils/common.ts
@@ -4,7 +4,7 @@
 
 const common = {
 	// Domain 
-	domain: process.env.NEXT_PUBLIC_BASE_URL ?? "https://beta.sandsofarrakis.me/",
+	domain: process.env.NEXT_PUBLIC_BASE_URL ?? "https://vana-dune-app.vercel.vana.com/",
 
 	// API Url
 	api_url: process.env.NEXT_PUBLIC_API_URL ?? "https://beta.sandsofarrakis.me/api",
