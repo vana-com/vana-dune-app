18d25bebf2548d70fb5e2ef4dbfbd996e4a52a3f
keno <keno@thinkbitsolutions.com>
2024-03-18 14:00:31 +0800

    test: for fonts fixes

--- app/api/getBalance/route.ts
+++ app/api/getBalance/route.ts
@@ -6,12 +6,19 @@ import { data } from ".."
 import { api } from "../api"
 export async function GET() {
 
+	let data: any = {
+		"status": true,
+		"balance": 126,
+	};
+
 	// let data: any = {
-	// 	"status": true,
-	// 	"balance": 1000,
+	// 	"error": {
+	// 		"status": 0,
+	// 		"statusText": "Error requesting https://backend.beta.sandsofarrakis.me/api/users/balance"
+	// 	}
 	// };
 
-	// return NextResponse.json(data)
+	return NextResponse.json(data)
 	const cookieStore = cookies()
 	const url = `${data.api_url}${api.getBalance}`;
 
--- app/api/getPrompt/route.ts
+++ app/api/getPrompt/route.ts
@@ -6,27 +6,40 @@ import { data } from ".."
 import { api } from "../api"
 export async function GET() {
 
-	// // when fail
+
+	const delay = (ms: any) => new Promise(res => setTimeout(res, ms));
+	// await delay(process.env.NEXT_PUBLIC_LOADING_TIME)
+	await delay(5000)
+
+	// when fail
 	// let data: any = {
 	// 	"status": false,
-	// 	"isCharacterError": true,
-	// 	message: "HTTP request returned status code 404:\n{\"statusCode\":404,\"name\":\"ModelNotFoundException\",\"message\":\"Character 9667bf0b-061d-4816-9dd5-a8c40a848747 does not hav (truncated...)\n",
+	// 	"isCharacter": true,
+	// 	"message": "HTTP request returned status code 404:\n{\"statusCode\":404,\"name\":\"ModelNotFoundException\",\"message\":\"Character 9667bf0b-061d-4816-9dd5-a8c40a848747 does not hav (truncated...)\n",
 	// };
 
-	// return NextResponse.json(data)
 
-
-	// const delay = (ms: any) => new Promise(res => setTimeout(res, ms));
-	// await delay(process.env.NEXT_PUBLIC_LOADING_TIME)
 	// let data: any = {
-	// 	"status": true,
-	// 	"story": "You're a Fremen, I'll name you \"Kuya Mara\". Like the Fremen, you're skilled in survival, thriving in life's harshest conditions. Your love for your family mirrors their strong community bonds, and your curiosity parallels their keen sense of awareness in the vast desert of life.",
-	// 	"tribe": "Fremen",
-	// 	"image_job_id": "780e7aa9-41db-4ed1-b3a0-850b013d20cc",
-	// 	"image_file": "https://storage.googleapis.com/vana-gotchi-jobs-development/780e7aa9-41db-4ed1-b3a0-850b013d20cc%2Fout-0.png"
-	// };
+	// 	"status": false,
+	// 	"message": "HTTP request returned status code 500:\n{\"name\":\"Error\",\"message\":\"@vercel/edge-config: Edge Config not found\",\"errors\":[\"@vercel/edge-config: Edge Config not f (truncated...)\n"
+	// }
 
-	// return NextResponse.json(data)
+	// let data: any = {
+	// 	"error": {
+	// 		"status": 0,
+	// 		"statusText": "Error requesting https://backend.beta.sandsofarrakis.me/api/users/generate-prompt"
+	// 	}
+	// }
+
+	let data: any = {
+		"status": true,
+		"story": "You're a Fremen, I'll name you \"Kuya Mara\". Like the Fremen, you're skilled in survival, thriving in life's harshest conditions. Your love for your family mirrors their strong community bonds, and your curiosity parallels their keen sense of awareness in the vast desert of life.",
+		"tribe": "Fremen",
+		"image_job_id": "780e7aa9-41db-4ed1-b3a0-850b013d20cc",
+		"image_file": "https://storage.googleapis.com/vana-gotchi-jobs-development/780e7aa9-41db-4ed1-b3a0-850b013d20cc%2Fout-0.png"
+	};
+
+	return NextResponse.json(data)
 
 	const cookieStore = cookies()
 	const url = `${data.api_url}${api.getPrompt}`;
