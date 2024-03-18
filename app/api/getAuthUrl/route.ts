9c3f7364555b06300019518e32064aa3d8c0915e
keno <keno@thinkbitsolutions.com>
2024-03-18 14:12:31 +0800

    rollback test code

--- app/api/getAuthUrl/route.ts
+++ app/api/getAuthUrl/route.ts
@@ -6,18 +6,18 @@ import { api } from "../api"
 
 export async function GET(req: Request, res: Response) {
 
-	// let temp_url = "http://localhost:3000/";
-	let temp_url = "https://beta.sandsofarrakis.me/";
+	// // let temp_url = "http://localhost:3000/";
+	// let temp_url = "https://beta.sandsofarrakis.me/";
 
-	// let temp_url = "https://34b6-180-191-16-36.ngrok-free.app/";
+	// // let temp_url = "https://34b6-180-191-16-36.ngrok-free.app/";
 
-	let data: any = {
-		"status": true,
-		"url": temp_url+"questions?code=ory_ac_0fpfAh3e0_M6U59tpbw8s-zJhmdyNsCXJyPjhVd9mNM.LJhZBqZ0NoMQNvhANCrQjcSXtE_f0t77GvOnlhHGzgo&scope=openid+offline&state=8f83f35d89a1c02b730de6296339956e",
-		"code_verifier": "ory_ac_0fpfAh3e0_M6U59tpbw8s-zJhmdyNsCXJyPjhVd9mNM.LJhZBqZ0NoMQNvhANCrQjcSXtE_f0t77GvOnlhHGzgo"
-	};
+	// let data: any = {
+	// 	"status": true,
+	// 	"url": temp_url+"questions?code=ory_ac_0fpfAh3e0_M6U59tpbw8s-zJhmdyNsCXJyPjhVd9mNM.LJhZBqZ0NoMQNvhANCrQjcSXtE_f0t77GvOnlhHGzgo&scope=openid+offline&state=8f83f35d89a1c02b730de6296339956e",
+	// 	"code_verifier": "ory_ac_0fpfAh3e0_M6U59tpbw8s-zJhmdyNsCXJyPjhVd9mNM.LJhZBqZ0NoMQNvhANCrQjcSXtE_f0t77GvOnlhHGzgo"
+	// };
 
-	return NextResponse.json(data)
+	// return NextResponse.json(data)
 
 	const url = `${data.api_url}${api.getAuthUrl}`;
 	try {
--- app/api/getSavedPrompt/[userID]/route.ts
+++ app/api/getSavedPrompt/[userID]/route.ts
@@ -6,20 +6,20 @@ import { api } from "../../api"
 
 export async function GET(req: Request, { params }: any) {
 
-		let story = "You'd be a part of the Rakian Priesthood, named \"Father Ormiston.\" A spiritual leader, you're known for your affirming nature, often comforting the flock with tales of survival and resilience in Arrakis' harsh desert. Despite occasional memory lapses, your strong sense of duty guides you, ensuring the worship of sandworms as divine remains unwavering."
+	// 	let story = "You'd be a part of the Rakian Priesthood, named \"Father Ormiston.\" A spiritual leader, you're known for your affirming nature, often comforting the flock with tales of survival and resilience in Arrakis' harsh desert. Despite occasional memory lapses, your strong sense of duty guides you, ensuring the worship of sandworms as divine remains unwavering."
 		
-		let data: any = {
-	    "status": true,
-	    "story": `${story}`,
-	    "tribe": "Rakian Priesthood",
-	    "quote": "\"*Amidst the shifting sands of Arrakis, Father Ormiston embarks on a profound pilgrimage, seeking the elusive Rakian Priesthood, his own tribe.*\"",
-	    "image_job_id": "ce11ef38-5bbb-4b5e-8ebd-c100f87d8fbd",
-	    // "image": "https://storage.googleapis.com/vana-gotchi-jobs-development/ce11ef38-5bbb-4b5e-8ebd-c100f87d8fbd%2Fout-0.png"
-	    "image": "https://storage.googleapis.com/vana-gotchi-jobs-development/780e7aa9-41db-4ed1-b3a0-850b013d20cc%2Fout-0.png"
+	// 	let data: any = {
+	//     "status": true,
+	//     "story": `${story}`,
+	//     "tribe": "Rakian Priesthood",
+	//     "quote": "\"*Amidst the shifting sands of Arrakis, Father Ormiston embarks on a profound pilgrimage, seeking the elusive Rakian Priesthood, his own tribe.*\"",
+	//     "image_job_id": "ce11ef38-5bbb-4b5e-8ebd-c100f87d8fbd",
+	//     // "image": "https://storage.googleapis.com/vana-gotchi-jobs-development/ce11ef38-5bbb-4b5e-8ebd-c100f87d8fbd%2Fout-0.png"
+	//     "image": "https://storage.googleapis.com/vana-gotchi-jobs-development/780e7aa9-41db-4ed1-b3a0-850b013d20cc%2Fout-0.png"
 
-	};
+	// };
 
-	return NextResponse.json(data)
+	// return NextResponse.json(data)
 
 	const user_id = params.userID;
 	const url = `${data.api_url}${api.getSavedPrompt}/${user_id}`;
